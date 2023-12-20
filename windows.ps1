7c77a07b185e7cea464344245ec4096f436c1ff4
FranciscoBanegas <fbanegas85@gmail.com>
2023-12-19 23:49:02 -0300

    Fomulario #1

---
+++ windows.ps1
@@ -0,0 +1,25 @@
+#add library
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+#Create Forms
+$win = New-Object System.Windows.Forms.Form
+$win.Text = 'Mi primer formulario'
+$win.ClientSize = '500,500'
+$win.BackColor = "#202020"
+$win.ForeColor = "#ffffff"
+
+
+#Add labels
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(140,80)
+$label.Text = 'Mi primer label'
+$label.AutoSize = $true
+
+#add from
+$win.Controls.Add($label)
+
+
+#show form
+$win.ShowDialog()
+$win.Dispose()
